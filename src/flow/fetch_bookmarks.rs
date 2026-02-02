cbbb7d180808842f606cda53352219295e67f720
Henry Blanchette <henry@monkeyface.com>
2026-02-02 16:40:01 -0500

    [checkpoint]

--- src/flow/fetch_bookmarks.rs
+++ src/flow/fetch_bookmarks.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 
-/// Fetch the bookmark urls via [`crate::utility::url_list::read_bookmarks_url_list`], then insert those urls into the database.
+/// Fetch the bookmark urls via [`crate::utility::url_list::read_bookmarks_url_list`], then insert those urls via [`crate::db::models::NewDocument`] into the `documents` table in the database. Only initialize these fields:
+/// - `url`
+/// - `added_date`
 pub async fn fetch_bookmarks() -> Result<()> {
     unimplemented!()
 }
