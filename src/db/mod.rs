13ab2d403abd124afb0dd9ce72d923233950115b
Henry Blanchette <henry@monkeyface.com>
2026-02-02 14:29:44 -0500

    [checkpoint]

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,12 +1,14 @@
 use crate::config::DATABASE_URL;
 use diesel::{connection::SimpleConnection, prelude::*};
+use diesel_migrations::{EmbeddedMigrations, embed_migrations};
+
 #[cfg(test)]
 use diesel_migrations::MigrationHarness;
-use diesel_migrations::{EmbeddedMigrations, embed_migrations};
 
 pub mod models;
 pub mod queries;
 pub mod schema;
+
 #[cfg(test)]
 mod test;
 
@@ -16,6 +18,14 @@ pub fn establish_connection() -> SqliteConnection {
     establish_common_connection(DATABASE_URL)
 }
 
+#[cfg(test)]
+pub fn establish_test_connection() -> SqliteConnection {
+    let mut conn = establish_common_connection(":memory:");
+    MigrationHarness::run_pending_migrations(&mut conn, MIGRATIONS)
+        .expect("Error running migrations");
+    conn
+}
+
 pub fn establish_common_connection(database_url: &str) -> SqliteConnection {
     let mut conn = SqliteConnection::establish(database_url).expect("Error connecting to database");
 
@@ -23,13 +33,5 @@ pub fn establish_common_connection(database_url: &str) -> SqliteConnection {
     conn.batch_execute("PRAGMA foreign_keys = ON")
         .expect("Error enabling foreign keys");
 
-    conn
-}
-
-#[cfg(test)]
-pub fn establish_test_connection() -> SqliteConnection {
-    let mut conn = establish_common_connection(":memory:");
-    MigrationHarness::run_pending_migrations(&mut conn, MIGRATIONS)
-        .expect("Error running migrations");
     conn
 }
